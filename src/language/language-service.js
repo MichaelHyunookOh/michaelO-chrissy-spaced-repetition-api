1129b9106c2427c9679bf9be9cac882d573cbf7d
Michaelhyunookoh <Michaelhyunookoh@gmail.com>
2020-09-01 13:35:59 -0700

    /head endpoint

--- src/language/language-service.js
+++ src/language/language-service.js
@@ -1,33 +1,47 @@
 const LanguageService = {
   getUsersLanguage(db, user_id) {
     return db
-      .from('language')
+      .from("language")
       .select(
-        'language.id',
-        'language.name',
-        'language.user_id',
-        'language.head',
-        'language.total_score',
+        "language.id",
+        "language.name",
+        "language.user_id",
+        "language.head",
+        "language.total_score"
       )
-      .where('language.user_id', user_id)
-      .first()
+      .where("language.user_id", user_id)
+      .first();
   },
 
   getLanguageWords(db, language_id) {
     return db
-      .from('word')
+      .from("word")
       .select(
-        'id',
-        'language_id',
-        'original',
-        'translation',
-        'next',
-        'memory_value',
-        'correct_count',
-        'incorrect_count',
+        "id",
+        "language_id",
+        "original",
+        "translation",
+        "next",
+        "memory_value",
+        "correct_count",
+        "incorrect_count"
       )
-      .where({ language_id })
+      .where({ language_id });
   },
-}
 
-module.exports = LanguageService
+  getNextWord(db, head) {
+    return db
+      .from("word")
+      .select("original", "correct_count", "incorrect_count")
+      .where("word.id", head)
+      .then((word) => {
+        return {
+          nextWord: word[0].original,
+          wordCorrectCount: word[0].correct_count,
+          wordIncorrectCount: word[0].incorrect_count,
+        };
+      });
+  },
+};
+
+module.exports = LanguageService;
---
+++ src/utils/LinkedLists.js
@@ -0,0 +1,107 @@
+class _Node {
+  constructor(value, next, prev) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  insertFirst(item) {
+    this.head = new _Node(item, null);
+  }
+  insertLast(item) {
+    if (this.head === null) this.insertFirst(item);
+    else {
+      let tempNode = this.head;
+      while (tempNode.next !== null) tempNode = tempNode.next;
+      tempNode.next = new _Node(item, null);
+    }
+  }
+  remove(item) {
+    if (!this.head) return null;
+    if (this.head.value === item) {
+      this.head = this.head.next;
+      return;
+    }
+    let currNode = this.head;
+    let previousNode = this.head;
+    while (currNode !== null && currNode.value !== item) {
+      previousNode = currNode;
+      currNode = currNode.next;
+    }
+    if (currNode === null) {
+      return;
+    }
+    previousNode.next = currNode.next;
+  }
+  find(item) {
+    let currNode = this.head;
+    if (!this.head) {
+      return null;
+    }
+    while (currNode.value !== item) {
+      if (currNode.next === null) {
+        return null;
+      } else {
+        currNode = currNode.next;
+      }
+    }
+    return currNode;
+  }
+  insertBefore(item, key) {
+    if (this.head === null) {
+      return null;
+    }
+    let currNode = this.head;
+    let prevNode = this.head;
+    while (currNode.value !== key && currNode !== null) {
+      prevNode = currNode;
+      currNode = currNode.next;
+    }
+    if (currNode === null) {
+      return;
+    }
+    prevNode.next = new _Node(item, currNode);
+  }
+  insertAfter(item, key) {
+    if (this.head === null) {
+      return null;
+    }
+    let currNode = this.head;
+    while (currNode.value !== key && currNode !== null) {
+      currNode = currNode.next;
+    }
+    if (currNode === null) {
+      return;
+    }
+    let tempNode = currNode.next;
+    currNode.next = new _Node(item, tempNode);
+  }
+
+  insertAt(itemToInsert, nthPosition) {
+    if (nthPosition < 0) {
+      throw new Error("Position error");
+    }
+    if (nthPosition === 0) {
+      this.insertFirst(itemToInsert);
+    } else {
+      const node = this._findNthElement(nthPosition - 1);
+      const newNode = new _Node(itemToInsert, null);
+      newNode.next = node.next;
+      node.next = newNode;
+    }
+  }
+
+  _findNthElement(position) {
+    let node = this.head;
+    for (let i = 0; i < position; i++) {
+      node = node.next;
+    }
+    return node;
+  }
+}
+
+module.exports = { LinkedList };
